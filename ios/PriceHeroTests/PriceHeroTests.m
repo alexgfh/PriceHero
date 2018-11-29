285977556009aadb40c8448bb620723aea69f949
Alex <alexgfh@gmail.com>
2018-11-29 17:34:54 +0200

    Start from react-native-firebase template

--- ios/PriceHeroTests/PriceHeroTests.m
+++ ios/PriceHeroTests/PriceHeroTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RNFirebaseStarterTests : XCTestCase
+@interface PriceHeroTests : XCTestCase
 
 @end
 
-@implementation RNFirebaseStarterTests
+@implementation PriceHeroTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
