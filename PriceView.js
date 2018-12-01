c350509671fd7a3048b0a27748756311d2fb9207
Alex <alexgfh@gmail.com>
2018-12-01 09:49:44 +0200

    Initial front-end work

---
+++ PriceView.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
+
+import firebase from 'react-native-firebase';
+
+import PriceChart from './PriceChart.js'
+
+class PriceView extends React.Component {
+	
+	
+  render() {
+	  let params = this.props.navigation.state.params;
+    return (
+    <View>
+		<ListItem
+        leftAvatar={{ source: { uri: params.avatar }, size:'large' }}
+        title={params.name}
+      />
+      <PriceChart/>
+      
+	</View>
+    )
+  }
+}
+
+export default withNavigation(PriceView);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  logo: {
+    height: 120,
+    marginBottom: 16,
+    marginTop: 64,
+    padding: 10,
+    width: 135,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  modules: {
+    margin: 20,
+  },
+  modulesHeader: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  module: {
+    fontSize: 14,
+    marginTop: 4,
+    textAlign: 'center',
+  }
+});
---
+++ ProductList.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
+
+import firebase from 'react-native-firebase';
+
+
+const products = [
+ {
+	EAN: '741414744622',
+    name: 'Plasma TV',
+    avatar: 'https://images.barcodelookup.com/6000/60002272-1.jpg'
+ },
+ {
+	EAN: '6921815603627',
+    name: 'One Plus 5',
+    avatar: 'https://images.barcodelookup.com/8662/86625814-1.jpg'
+ },
+ {
+	EAN: '617529207942',
+    name: 'Nikon L320',
+    avatar: 'https://images.barcodelookup.com/219/2194269-1.jpg'
+ },
+]
+
+class ProductList extends React.Component {
+	
+	
+  render() {
+    return (
+      <View>
+  {
+    products.map((l, i) => (
+      <ListItem
+        key={i}
+        leftAvatar={{ source: { uri: l.avatar }, size:'large' }}
+        title={l.name}
+        onPress={()=>this.props.navigation.navigate('Price', {productSelected: l.EAN, name: l.name, avatar:l.avatar})}
+      />
+    ))
+  }
+</View>
+    );
+  }	
+}
+
+export default withNavigation(ProductList);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  logo: {
+    height: 120,
+    marginBottom: 16,
+    marginTop: 64,
+    padding: 10,
+    width: 135,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  modules: {
+    margin: 20,
+  },
+  modulesHeader: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  module: {
+    fontSize: 14,
+    marginTop: 4,
+    textAlign: 'center',
+  }
+});
